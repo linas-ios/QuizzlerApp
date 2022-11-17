51a44427cad6c72a58f526b4c286aef2cee9d62f
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-11-17 23:17:47 +0100

    create array with questions create func update Label view

--- QuizzlerApp/ViewController.swift
+++ QuizzlerApp/ViewController.swift
@@ -8,18 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  //MARK: Outlets
+  
   @IBOutlet weak var scoreLabel: UILabel!
   @IBOutlet weak var questionLabel: UILabel!
   @IBOutlet weak var trueUIButton: UIButton!
   @IBOutlet weak var falseUIButton: UIButton!
   
+  //MARK: Properties
+  
+  let quiz = [
+    "For + Two is equal to Six",
+    "Five - Three is greater than One",
+    "Three + Eight is less than Ten"
+  ]
+  //Tracking question number. It's mean we check which currenty question now user reading
+  var questionNumber = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    updateUI()
   }
 
+  //MARK: Actions
+  
   @IBAction func answerButtonTapped(_ sender: UIButton) {
+    questionNumber += 1
+    updateUI()
+  }
+  
+  //MARK: Functions
+  
+  func updateUI() {
+    //[questionNumber] inside array quiz
+    questionLabel.text = quiz[questionNumber]
   }
   
 }
