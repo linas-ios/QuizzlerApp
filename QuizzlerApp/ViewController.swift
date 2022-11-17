3ea34eae15b2b2aadbff4c73d571a6d5821c4c1c
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-11-17 22:58:20 +0100

    Create UI
    add label
    add buttons
    add progressBar

--- QuizzlerApp/ViewController.swift
+++ QuizzlerApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var questionLabel: UILabel!
+  @IBOutlet weak var trueUIButton: UIButton!
+  @IBOutlet weak var falseUIButton: UIButton!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func answerButtonTapped(_ sender: UIButton) {
+  }
+  
 }
 
